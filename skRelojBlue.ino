1ba4e69ba2064d0ef00c67291bca6b1a6e56a568
JAVI1966 <JEG1966@telefonica.net>
2016-05-29 12:06:32 +0200

    alarma
    inclusion de alarma

--- skRelojBlue.ino
+++ skRelojBlue.ino
@@ -25,22 +25,19 @@ unsigned char halfsecond = 0;
 unsigned char second = 0;
 unsigned char minuto = 0;
 unsigned char hora = 12;
+unsigned char sec_alarma=0;
 unsigned char minuto_alarma = 0;
 unsigned char hora_alarma = 0;
+int beep_count=0;
+
 bool toggle = false;
 int16_t i = 0;
 String strDato = "";
 String aux = "";
 bool _DEBUG_ = true;
 
-
-
-
 TM1637 tm1637(CLK, DIO);
 SoftwareSerial BT(8, 9); //RX TX
-
-
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -61,38 +58,56 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-
   if (Update == ON)
   {
-
-
     TimeUpdate();
     tm1637.display(TimeDisp);
 
+   
+     if(DS3231_triggered_a1()){
+   
+      
+      if(++beep_count < 120){  //aprox minuto y medio
+        
+        if(!toggle)
+           tm1637.set(0);
+         else
+           tm1637.set(BRIGHTEST);  
+ 
+      }
+      else
+         {
+          tm1637.set(BRIGHTEST); 
+          beep_count =0;
+          hora_alarma = 0;
+          minuto_alarma = 0;
+          DS3231_clear_a1f();
+          
+         }
 
-
-    if (hora == hora_alarma && minuto == minuto_alarma)
+     
+      
       if (_DEBUG_)
-        Serial.print("!!!!!!!!!!ALARMA !!!!!!!!!!!!");
+        
+             Serial.print("!!!!!!!!!!ALARMA !!!!!!!!!!!!\r\n");
 
-  }
-
+    }//if disparo alarma
+ 
   if (BT.available() > 0) {
 
 
     // read the incoming byte:
     strDato = BT.readString();
 
-    if (strDato.length() < 4 || strDato.length() > 9) {
+    if ( ! strDato.length() && strDato.length() > 9) {
       if (_DEBUG_)
-        Serial.print("Incorrecto: " + strDato + "\n");
+        Serial.print("Incorrecto: " + strDato + "\r\n");
       return;
     }
 
     if (strDato.startsWith("A"))
     {
-      if (_DEBUG_)
-        Serial.print("Hora alarma: " );
+      
 
       if (strDato.length() == 5) {
 
@@ -105,24 +120,28 @@ void loop() {
 
       }
       if (_DEBUG_) {
+        Serial.print("Hora alarma: ");
         Serial.print(hora_alarma);
+        Serial.print(":");
         Serial.print(minuto_alarma);
+        Serial.print("\r\n");
       }
+      
+      setAlarma();
       displayAlarma();
+      
     }
     else if (strDato.startsWith("B")) {
-
-      hora_alarma = 0;
-      minuto_alarma = 0;
+         tm1637.set(BRIGHTEST); 
+         hora_alarma = 0;
+         minuto_alarma = 0;
+         sec_alarma=0;
+         DS3231_clear_a1f();
 
       if (_DEBUG_)
-        Serial.print("Alarma Borrada");
-
-
+        Serial.print("Alarma Borrada\r\n");
     }
-
     else {
-
       struct ts t;
 
       if (strDato.length() == 7) {
@@ -130,15 +149,12 @@ void loop() {
         t.min = strDato.substring(3, 5).toInt();
         t.hour = strDato.substring(0, 2).toInt();
 
-        //minuto = strDato.substring(2, 4).toInt();
-        //hora = strDato.substring(0, 2).toInt();
       }
       else if (strDato.length() == 8) {
         t.sec = strDato.substring(6,8).toInt();
         t.min = strDato.substring(3,5).toInt();
         t.hour = strDato.substring(0, 2).toInt();
-        //minuto = strDato.substring(3, 5).toInt();
-        //hora = strDato.substring(0, 2).toInt();
+       
       }
 
       DS3231_set(t);
@@ -148,10 +164,12 @@ void loop() {
 
     // say what you got:
     if (_DEBUG_)
-      Serial.print("Recibido: " + strDato + "\n");
+      Serial.print("Recibido: " + strDato + "\r\n");
 
 
-  }
+   }// if (BT.available() > 0)
+
+  }//if (Update == ON)
 
 
 }
@@ -164,27 +182,9 @@ void TimingISR()
   Update = ON;
   toggle = !toggle;
   digitalWrite(LED, toggle);
-
-
-  if (halfsecond == 2) {
-
-    second ++;
-    if (second == 60)
-    {
-      minuto ++;
-      if (minuto == 60)
-      {
-        hora ++;
-        if (hora == 24)hora = 0;
-        minuto = 0;
-      }
-      second = 0;
-    }
-    halfsecond = 0;
-  }
+ 
+  
   // Serial.println(second);
-
-
 }
 //********************************************************************************
 void TimeUpdate(void)
@@ -199,18 +199,11 @@ void TimeUpdate(void)
   //  else tm1637.point(POINT_OFF);
   tm1637.set_decpoint(1);
 
-
   TimeDisp[0] = t.hour / 10;
   TimeDisp[1] = t.hour % 10;
   TimeDisp[2] = t.min / 10;
   TimeDisp[3] = t.min % 10;
-
-  /*
-    TimeDisp[0] = hora / 10;
-    TimeDisp[1] = hora % 10;
-    TimeDisp[2] = minuto / 10;
-    TimeDisp[3] = minuto % 10;*/
-
+ 
 }
 //*******************************************************************************
 void displayAlarma(void)
@@ -223,10 +216,54 @@ void displayAlarma(void)
   TimeDisp[2] = minuto_alarma / 10;
   TimeDisp[3] = minuto_alarma % 10;
   tm1637.display(TimeDisp);
-  delay(1000 * 5);
+  delay(1000 * 4);
   tm1637.set(BRIGHTEST);
   Timer1.restart();
 
+}
+//********************************************************************************
+void setAlarma()
+{
+
+    // flags define what calendar component to be checked against the current time in order
+    // to trigger the alarm - see datasheet
+    // A1M1 (seconds) (0 to enable, 1 to disable)
+    // A1M2 (minutes) (0 to enable, 1 to disable)
+    // A1M3 (hour)    (0 to enable, 1 to disable) 
+    // A1M4 (day)     (0 to enable, 1 to disable)
+    // DY/DT          (dayofweek == 1/dayofmonth == 0)
+    byte flags[5] = { 0, 0, 0, 1, 1 }; //Set alarm to trigger every 24 hours on time match
+
+    // set Alarm1
+    DS3231_set_a1(0, minuto_alarma, hora_alarma, 0, flags); //Set alarm 1 RTC registers
+
+}
+//************************************************************************************
+void get_alarm()
+{
+    uint8_t n[4];
+    uint8_t t[4];               //second,minute,hour,day
+    uint8_t f[5];               // flags
+    uint8_t i;
+
+    Wire.beginTransmission(DS3231_I2C_ADDR);
+    Wire.write(DS3231_ALARM1_ADDR);
+    Wire.endTransmission();
+
+    Wire.requestFrom(DS3231_I2C_ADDR, 4);
+
+    for (i = 0; i <= 3; i++) {
+        n[i] = Wire.read();
+        f[i] = (n[i] & 0x80) >> 7;
+        t[i] = bcdtodec(n[i] & 0x7F);
+    }
+
+    f[4] = (n[3] & 0x40) >> 6;
+    t[3] = bcdtodec(n[3] & 0x3F);
+    
+    sec_alarma = t[0];
+    minuto_alarma = t[1];
+    hora_alarma = t[2];
 }
 
 
